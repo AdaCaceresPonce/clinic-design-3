103f1c1accc8ec6c0742c073ce045f714e369e5b
everthdc <145787218+everthdc@users.noreply.github.com>
2024-04-06 22:36:06 -0500

    fix/Cambiar textarea y modificar imagenes del index servicios

--- resources/views/admin/services/create.blade.php
+++ resources/views/admin/services/create.blade.php
@@ -14,7 +14,7 @@
 
     <form action="{{ route('admin.services.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="card-gray">
+        <div class="card-gray mx-auto max-w-[1230px]">
             {{-- Imagenes --}}
             <div class="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
                 <div class="col lg:mb-0">
@@ -27,15 +27,16 @@
                                 class="flex items-center px-2.5 py-1.5 lg:px-4 lg:py-2 rounded-lg btn-blue cursor-pointer text-sm lg:text-base">
                                 <i class="fas fa-camera mr-2"></i>
                                 Actualizar imagen
-                                <input id="uploadImage1" name="card_image_path" type="file" class="hidden" accept="image/*"
-                                    onchange="previewImage(1);" />
+                                <input id="uploadImage1" name="card_image_path" type="file" class="hidden"
+                                    accept="image/*" onchange="previewImage(1);" />
                             </label>
                         </div>
-                        <img id="uploadPreview1" class="object-contain w-full max-h-48 md:max-h-[362px] rounded-xl"
+                        <img id="uploadPreview1"
+                            class="object-contain w-full max-h-48 md:max-h-[365px] md:min-h-[365px] border-[2px] bg-white border-blue-400 rounded-xl"
                             src="{{ asset('img/no-image.jpg') }}" alt="">
                     </figure>
                     {{-- Alerta de validacion --}}
-                    <x-input-error for="card_image_path"/>
+                    <x-input-error for="card_image_path" />
                 </div>
 
                 <div class="col">
@@ -48,15 +49,16 @@
                                 class="flex items-center px-2.5 py-1.5 lg:px-4 lg:py-2 rounded-lg btn-blue cursor-pointer text-sm lg:text-base">
                                 <i class="fas fa-camera mr-2"></i>
                                 Actualizar imagen
-                                <input id="uploadImage2" name="cover_image_path" type="file" class="hidden" accept="image/*"
-                                    onchange="previewImage(2);" />
+                                <input id="uploadImage2" name="cover_image_path" type="file" class="hidden"
+                                    accept="image/*" onchange="previewImage(2);" />
                             </label>
                         </div>
-                        <img id="uploadPreview2" class="object-contain w-full max-h-48 md:max-h-[362px] rounded-xl"
+                        <img id="uploadPreview2"
+                            class="object-contain w-full max-h-48 md:max-h-[365px] md:min-h-[365px] border-[2px] bg-white border-blue-400 rounded-xl"
                             src="{{ asset('img/no-image.jpg') }}" alt="">
                     </figure>
                     {{-- Alerta de validacion --}}
-                    <x-input-error for="cover_image_path"/>
+                    <x-input-error for="cover_image_path" />
                 </div>
             </div>
 
@@ -67,30 +69,28 @@
                 </x-label>
                 <x-input class="w-full" placeholder="Ingrese el nombre del servicio" name="name"
                     value="{{ old('name') }}" />
-                <x-input-error for="name"/>
+                <x-input-error for="name" />
             </div>
 
             <div class="mb-4">
                 <x-label class="mb-1">
                     Descripción para la tarjeta:
                 </x-label>
-                <x-textarea class="w-full" placeholder="Ingrese una descripción muy breve del servicio"
-                    name="small_description">
-                    {{ old('small_description') }}
-                </x-textarea>
-                <x-input-error for="small_description"/>
-                
+                <textarea name="small_description"
+                    class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                    placeholder="Ingrese una descripción muy breve del servicio" name="">{{ old('small_description') }}</textarea>
+                <x-input-error for="small_description" />
+
             </div>
 
             <div class="mb-4">
                 <x-label class="mb-1">
                     Descripción del servicio:
                 </x-label>
-                <x-textarea class="w-full" placeholder="Ingrese la descripción general del servicio"
-                    name="long_description">
-                    {{ old('long_description') }}
-                </x-textarea>
-                <x-input-error for="long_description"/>
+                <textarea name="long_description"
+                    class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                    placeholder="Ingrese la descripción completa del servicio" name="">{{ old('long_description') }}</textarea>
+                <x-input-error for="long_description" />
 
             </div>
 
@@ -98,11 +98,9 @@
                 <x-label class="mb-1">
                     Información adicional del servicio:
                 </x-label>
-                <x-textarea class="w-full"
-                    placeholder="Puedes detallar más información adicional del servicio (Opcional)"
-                    name="additional_info">
-                    {{ old('additional_info') }}
-                </x-textarea>
+                <textarea name="additional_info"
+                    class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+                    placeholder="Puedes detallar más información adicional del servicio (Opcional)" name="">{{ old('additional_info') }}</textarea>
             </div>
 
             <div class="flex justify-end">
