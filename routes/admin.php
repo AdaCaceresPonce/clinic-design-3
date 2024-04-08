207a1f2b244395169c23167f31d394f37919dc9a
everthdc <145787218+everthdc@users.noreply.github.com>
2024-04-08 05:10:22 -0500

    Añadir ruta de especialidades

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\ServiceController;
+use App\Http\Controllers\Admin\SpecialtyController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,4 @@ Route::get('/', function () {
 })->name('dashboard');
 
 Route::resource('services', ServiceController::class);
+Route::resource('specialties', SpecialtyController::class);
