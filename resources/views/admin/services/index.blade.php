a2c755256dc6e539d4d1a98e7ff5cec4f605bbbe
everthdc <145787218+everthdc@users.noreply.github.com>
2025-09-16 20:37:23 -0500

    Culminar funcionalidad de eliminar servicio desde index, y se hace pequeños cambios en los botones de su crud

--- resources/views/admin/services/index.blade.php
+++ resources/views/admin/services/index.blade.php
@@ -23,9 +23,17 @@
 
 
     <x-slot name="action">
-        <a class="btn btn-blue" href="{{ route('admin.services.create') }}">
+        <x-wireui-button href="{{ route('admin.services.create') }}" blue>
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                <path fill-rule="evenodd"
+                    d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14Zm.75-10.25v2.5h2.5a.75.75 0 0 1 0 1.5h-2.5v2.5a.75.75 0 0 1-1.5 0v-2.5h-2.5a.75.75 0 0 1 0-1.5h2.5v-2.5a.75.75 0 0 1 1.5 0Z"
+                    clip-rule="evenodd" />
+            </svg>
+
             Nuevo
-        </a>
+
+        </x-wireui-button>
     </x-slot>
 
     @livewire('admin.datatables.service-table')
@@ -40,5 +48,35 @@
 
     </div> --}}
 
+    @push('js')
+        <script>
+            //Seleccionar todos los forms que tengan la siguiente clase
+            forms = document.querySelectorAll('.delete-form');
+
+            forms.forEach(form => {
+                form.addEventListener('submit', function(e) {
+                    e.preventDefault();
+
+                    Swal.fire({
+                        title: "¿Estás seguro?",
+                        text: "¡No podrás revertir esto!",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#3085d6",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "¡Sí, borralo!",
+                        cancelButtonText: "Cancelar"
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            form.submit();
+                        }
+
+                    });
+
+
+                });
+            });
+        </script>
+    @endpush
 
 </x-admin-layout>
