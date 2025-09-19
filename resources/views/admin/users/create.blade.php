9da9582df137ae7251301988ab8f4e35471116d3
everthdc <145787218+everthdc@users.noreply.github.com>
2025-09-19 16:30:01 -0500

    Crear vistas para el crud de Usuarios

---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,15 @@
+<x-admin-layout :breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'route' => route('admin.dashboard'),
+    ],
+    [
+        'name' => 'Usuarios',
+        'route' => route('admin.users.index'),
+    ],
+    [
+        'name' => 'Crear Nuevo',
+    ],
+]">
+
+</x-admin-layout>
---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,15 @@
+<x-admin-layout :breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'route' => route('admin.dashboard'),
+    ],
+    [
+        'name' => 'Usuarios',
+        'route' => route('admin.services.index'),
+    ],
+    [
+        'name' => 'Editar',
+    ],
+]">
+
+</x-admin-layout>
