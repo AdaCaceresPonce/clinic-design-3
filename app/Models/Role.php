19f91db1716f5e8e1eda47db3027cec545b08fd2
everthdc <145787218+everthdc@users.noreply.github.com>
2025-09-23 11:54:11 -0500

    Habilitar la asignacion masiva en los modelos que extienden a los de permisos y roles, para incluir correctamente los campos nuevos que se habian agregado

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Role as SpatieRole;
 
-class Role extends Model
+class Role extends SpatieRole
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'display_name',
+        'is_active',
+        'guard_name',
+    ];
 }
