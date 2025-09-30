e243908a350c24a383ed466353ba9e7d588d3aaa
everthdc <145787218+everthdc@users.noreply.github.com>
2025-09-30 02:43:33 -0500

    Se agrega en el formulario de opiniones la funcionalidad de valoración por estrellas, el cual usa el nuevo campo "rating"

--- resources/views/livewire/web/opinions/save-opinion.blade.php
+++ resources/views/livewire/web/opinions/save-opinion.blade.php
@@ -4,14 +4,14 @@
 
         {{-- Nombres y Apellidos --}}
         <div class="flex flex-col space-y-5 md:flex-row md:space-y-0 md:space-x-4 lg:flex-col lg:space-y-5 lg:space-x-0">
-            
+
             {{-- Nombres --}}
             <div class="flex-1">
                 <x-label class="mb-1 text-[15px] font-black">
                     Nombres:
                     <span class="text-red-500">*</span>
                 </x-label>
-                <x-input class="w-full" placeholder="Ingrese sus nombres" wire:model.live="opinion.name"/>
+                <x-input class="w-full" placeholder="Ingrese sus nombres" wire:model.live="opinion.name" />
                 <x-input-error for="opinion.name" />
             </div>
 
@@ -46,6 +46,37 @@
             <x-input-error for="opinion.service_id" />
         </div>
 
+        {{-- Valoración con Estrellas --}}
+        <div x-data="{
+            rating: @entangle('opinion.rating').live,
+            hover: 0
+        }">
+            <x-label class="mb-2 text-[15px] font-black">
+                Valoración:
+                <span class="text-red-500">*</span>
+            </x-label>
+
+            <div class="flex items-center">
+                <template x-for="star in 5" :key="star">
+                    <button type="button" @click="rating = star" @mouseenter="hover = star" @mouseleave="hover = 0"
+                        class="focus:outline-none transition-colors duration-150">
+
+                        <svg class="w-8 h-8 sm:w-9 sm:h-9"
+                            :class="star <= (hover || rating) ? 'text-yellow-400 fill-current' : 'text-yellow-400 fill-white'"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                        </svg>
+
+                    </button>
+                </template>
+
+                <span x-show="rating > 0" x-text="rating" class="flex align-middle pt-0.5 leading-none ml-2 text-lg font-semibold text-gray-700"></span>
+            </div>
+
+            <x-input-error for="opinion.rating" />
+        </div>
 
         {{-- Mensaje de opinión --}}
         <div>
@@ -71,9 +102,9 @@
                 </div>
 
                 <div wire:loading wire:loading.flex wire:target="save" class="flex items-center justify-center">
-                    Cargando 
-                    <svg aria-hidden="true" class="ml-2 w-5 h-5 animate-spin fill-slate-500"
-                        viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    Cargando
+                    <svg aria-hidden="true" class="ml-2 w-5 h-5 animate-spin fill-slate-500" viewBox="0 0 100 101"
+                        fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
                             fill="currentColor" />
