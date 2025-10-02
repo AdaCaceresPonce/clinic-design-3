73a1b8e16b755bb45baaf09410a5379942bd0742
AdaCaceresPonce <145631675+AdaCaceresPonce@users.noreply.github.com>
2025-10-02 11:42:18 -0500

    arreglo de estrellas en plomo oscuro y plomo griss

--- resources/views/livewire/web/sliders/slider-opinions.blade.php
+++ resources/views/livewire/web/sliders/slider-opinions.blade.php
@@ -39,21 +39,23 @@
                                 <div class="flex items-center gap-1">
                                     @for ($i = 1; $i <= 5; $i++)
                                         @if ($i <= $opinion->rating)
-                                            <svg class="w-5 h-5 text-yellow-400 fill-current"
+                                            {{-- Estrella amarilla --}}
+                                            <svg class="w-5 h-5 text-yellow-400" 
                                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                                <path
+                                                <path fill="currentColor" 
                                                     d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
                                             </svg>
+
                                         @else
-                                            <svg class="w-5 h-5 text-gray-300 fill-current"
+                                            {{-- Estrella gris --}}
+                                            <svg class="w-5 h-5 text-gray-300" 
                                                 xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                                <path
+                                                <path fill="currentColor" 
                                                     d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
                                             </svg>
                                         @endif
                                     @endfor
-                                    <span
-                                        class="text-sm font-semibold text-gray-700 ml-1">{{ $opinion->rating }}.0</span>
+                                    <span class="text-sm font-semibold text-gray-700 ml-1">{{ $opinion->rating }}.0</span>
                                 </div>
 
                                 {{-- Ícono de comillas --}}
