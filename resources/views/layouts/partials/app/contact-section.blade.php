44db41e8729ec13841689d656e39d423dbdc7436
AdaCaceresPonce <145631675+AdaCaceresPonce@users.noreply.github.com>
2025-10-12 10:53:56 -0500

    nuevo_diseño

--- resources/views/layouts/partials/app/contact-section.blade.php
+++ resources/views/layouts/partials/app/contact-section.blade.php
@@ -14,8 +14,9 @@
         {{-- Formulario --}}
         
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+
             {{-- Dirección --}}
-            <div class="bg-white shadow-[0_4px_6px_#C8F8FF] rounded-xl shadow-md p-6 text-center flex flex-col items-center hover:shadow-lg transition">
+            <div class="bg-white shadow-[0_4px_6px_#C8F8FF] rounded-xl shadow-md p-6 text-center flex flex-col items-center  transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
                 <div class="w-12 h-12 flex items-center justify-center rounded-full bg-gradient-to-r from-blue-500 to-teal-400 mb-3 text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 11c1.104 0 2-.896 2-2s-.896-2-2-2-2 .896-2 2 .896 2 2 2z" />
@@ -26,9 +27,9 @@
                     {{ $clinic_information['address'] ?? 'Dirección no disponible' }}
                 </p>
             </div>
-
+        
             {{-- Teléfono --}}
-            <div class="bg-gradient-to-r from-blue-600 to-teal-500 rounded-xl shadow-md p-6 text-center flex flex-col items-center text-white hover:shadow-lg transition">
+            <div class="bg-gradient-to-r from-blue-600 to-teal-500 rounded-xl shadow-md p-6 text-center flex flex-col items-center text-white hover:shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
                 <div class="w-12 h-12 flex items-center justify-center rounded-full bg-white text-blue-600 mb-3">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.516 4.547a1 1 0 01-.272 1.026l-2.2 2.2a11.04 11.04 0 005.198 5.198l2.2-2.2a1 1 0 011.026-.272l4.547 1.516a1 1 0 01.684.948V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
@@ -40,9 +41,9 @@
                     {{ data_get($clinic_information, 'phone', '') }}
                 </p>
             </div>
-
+        
             {{-- Email --}}
-            <div class="bg-white rounded-xl shadow-md p-6 text-center flex flex-col items-center hover:shadow-lg shadow-[0_4px_6px_#C8F8FF] transition">
+            <div class="bg-white rounded-xl shadow-md p-6 text-center flex flex-col items-center hover:shadow-lg shadow-[0_4px_6px_#C8F8FF] transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
                 <div class="w-12 h-12 flex items-center justify-center rounded-full bg-gradient-to-r from-blue-500 to-teal-400 mb-3 text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
@@ -52,9 +53,9 @@
                     {{ $clinic_information['email'] ?? 'Correo no disponible' }}
                 </p>
             </div>
-
+        
             {{-- Horarios --}}
-            <div class="bg-gradient-to-r from-blue-600 to-teal-500 rounded-xl shadow-md p-6 text-center flex flex-col items-center text-white hover:shadow-lg transition">
+            <div class="bg-gradient-to-r from-blue-600 to-teal-500 rounded-xl shadow-md p-6 text-center flex flex-col items-center text-white transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
                 <div class="w-12 h-12 flex items-center justify-center rounded-full bg-white text-blue-600 mb-3">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -64,6 +65,7 @@
                     {{ $clinic_information['schedule'] ?? 'Horario no disponible' }}
                 </p>
             </div>
+        
         </div>
                
         <div class="order-last lg:order-first  rounded-2xl  ">
