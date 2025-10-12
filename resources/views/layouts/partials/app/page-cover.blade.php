c0d00e70ca918b1e9cc9251d6813d044f923506e
everthdc <145787218+everthdc@users.noreply.github.com>
2025-10-12 01:46:50 -0500

    Ligero cambio de gradiente en la portada del resto de paginas

--- resources/views/layouts/partials/app/page-cover.blade.php
+++ resources/views/layouts/partials/app/page-cover.blade.php
@@ -1,12 +1,12 @@
-@props(['image' => '', 'name' => 'Sin título', 'id' => '#' ])
+@props(['image' => '', 'name' => 'Sin título', 'id' => '#'])
 
-<section class="w-full brightness-90 contrast-150 bg-cover bg-no-repeat bg-center relative"
-    style="background-image: url('{{ $image }}');"
+<section class="w-full bg-cover bg-no-repeat bg-center relative" style="background-image: url('{{ $image }}');"
     id="{{ $id }}">
-    <div class="bg-blue-900 bg-opacity-50 inset-0 absolute z-10">
-    </div>
-    <x-container class="px-4 py-44 lg:py-48 h-full flex align-middle items-center relative z-20">
-        <p class="text-white text-4xl sm:text-5xl font-bold border-l-[7px] pl-6 tracking-normal">
+
+    <div class="bg-blue-900/40 inset-0 absolute z-10"></div>
+
+    <x-container class="px-4 py-24 lg:py-48 h-full flex align-middle items-center relative z-20">
+        <p class="text-white text-3xl sm:text-5xl font-bold border-l-[7px] pl-6 tracking-normal">
             {{ $name }}
         </p>
     </x-container>
